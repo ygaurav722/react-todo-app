2bc70de6be1130e7e464802bd7ee3f677f5938cd
ygaurav722 <gauravyadav72201@gmail.com>
2022-09-14 12:10:28 +0530

    css tweaked

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,14 +2,14 @@ import React, { useState,useRef } from 'react'
 
 function TodoForm(props) {
     const [input,setInput]=useState(props.edit ? props.edit :'');
-    const inputRef=useRef(null);
+    const inputRef=useRef('');
     const handleInput=(e)=>{
         setInput(e.target.value);
     };
 
     const handleSubmit=e=>{
         e.preventDefault();
-
+        
         props.onSubmit({
             id: Math.floor(Math.random()*1000),
             text: input
@@ -29,7 +29,7 @@ function TodoForm(props) {
         </button>
         </>
         ):(
-          <><input type="text" placeholder=" Add a note..." value={input} className="todo-input" onChange={handleInput} ref={ inputRef}/>
+          <><input type="text" placeholder=" Add a note..." value={input} className="todo-input" onChange={handleInput} ref={inputRef}/>
         <button className="todo-button" >
         Add Todo
         </button></>
